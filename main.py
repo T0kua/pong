e3dd73752afdfc26474852f3a9bd6d65894d2333
Sergey Filippov <alekseiwvnf@gmail.com>
2025-01-15 20:02:37 +0300

    finish release

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import time
 import os
+import time
 import function
-
+from pynput import keyboard
 
 size = (80,15)
 player = 5 # p1x = 2 p2.x = size[0] -2
 playerSize = 2 # size = 1 + (pSize * 2)
-
+reward = 0
+Debug = function.get("debug")
+cc = 0
+file = open("json.txt","r")
+OS = function.get("OS")
 
 class point:
     def __init__(self,x,y):
@@ -24,8 +28,12 @@ class point:
         self.vector[0], self.vector[1] = - self.vector[0], -self.vector[0]
 
 
+p = point(10,10)
+
+
 def Draw(p : point, Debug="False") -> None:
     os.system(OS)
+    global reward
     for m in range(size[1]):
         print("|", end="")
         for n in range(size[0]):
@@ -38,24 +46,55 @@ def Draw(p : point, Debug="False") -> None:
         print("|", end="")
         if m == size[1] // 2 and Debug == "True" :
             print(f"VECTOR [{p.vector[0]}, {p.vector[1]} ]\tPOS - [x:{p.x}, y:{p.y}]", end="")
-        if (m - 1== (size[1] )// 2):
-            reward = 0
+        if (m - 1 == (size[1]) // 2):
             print(f"SPACE {"0" * (len(str(reward)) -1)}{reward}",end="")
+        if (m - 2 == (size[1]) // 2) and Debug == "True":
+            print(f"PLAYER {player} CC {cc  }")
         print("\n")
 
+def game_over() -> None :
+     global reward
+     print("game over")
+     if int(function.get("reward")) < reward:
+          author = None
+          while (author == None):
+               author = input("enter your name:")
+          function.set("reward", str(reward))
+          function.set("author", author)
 
+     exit()
 
 def Logick(p : point) -> None:
     p.metrix()
-    if p.x >= size[0] - 1 or p.x == 0 : p.vector[0] = - p.vector[0]
+    global reward
+    if p.x >= size[0] - 1 or p.x == 0 : p.vector[0] = game_over()#- p.vector[0]
     if p.y >= size[1] - 1 or p.y == 0 : p.vector[1] = -p.vector[1]
-    if (player - playerSize <= p.y  <= player + playerSize) and (p.x == 2 or p.x == size[0] - 2): p.bimp()
+    if (player - playerSize <= p.y  <= player + playerSize) and (p.x == 2 or p.x == size[0] - 2):
+         p.bimp()
+         reward += 1
 
+def Control(key):
+    global player
+    global cc
+    try:
+         key.char
+    except:
+         return 0
+    if key.char in ['w']:
+            if cc > 0 : cc = 0
+            cc -= 0.5
+    elif key.char in ['s']:
+            if cc < 0 : cc = 0
+            cc += 0.5
+    if player - playerSize < 0:
+         player = playerSize
+    if player + playerSize > size[1]:
+         player = size[1] - playerSize
+    player += (abs(cc) // 10) * (-1 if cc < 0 else 1) 
+    if abs(cc) > 10:
+        cc = 0  
+    
 
-Debug = function.get("debug")
-p = point(10,10)
-file = open("json.txt","r")
-OS = function.get("OS")
 while not(OS == "cls" or OS == "clear"):
         OS = int(input("enter your Operation system:\n1 - windows\t2 - *Unix"))
         match OS:
@@ -69,5 +108,7 @@ file.close()
 
 while True:
     Logick(p)
-    Draw(p,Debug)
-    time.sleep(0.5)
+    Draw(p, Debug)
+    listener = keyboard.Listener(on_release=Control)
+    time.sleep(0.2)
+    listener.start()
